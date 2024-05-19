9108bd92256613ad469be24cbf49622f3e135543
ChandikaSandun <chandikasandun123@gmail.com>
2024-05-19 20:57:53 +0530

    Add data into student_data.py

---
+++ student_data.py
@@ -0,0 +1,19 @@
+from student import Student
+from school import School
+from parent import Parent
+
+# Example usage
+if __name__ == "__main__":
+    # Creating a student instance
+    student1 = Student("A.M.C.S.Arachchi", "216005F")
+
+    # Creating a school instance
+    school1 = School("Central College Anuradhapura", "Anuradhapura")
+
+    # Creating a parent instance
+    parent1 = Parent(student1, "Sunil Gamini", "0772893387")
+
+    # Printing information
+    print(student1.get_student_info())
+    print(school1.get_school_info())
+    print(parent1.get_parent_info())
